81dbf66af293e01185b0228c399d486c20282ada
zeroxnone <62907451+zeroxnone@users.noreply.github.com>
2022-02-05 15:05:18 -0600

    Add files via upload
    Current SourceCode (Build1)

---
+++ main (1).py
@@ -0,0 +1,63 @@
+#beta test ui
+
+import PySimpleGUI as sim
+sim.theme('DarkTeal6')
+
+sitename = ""
+username = ""
+password = ""
+sitemirror = "" #mirrors to be turned into truncated strings as apposed to their tuple counterparts
+usermirror = ""
+passmirror = ""
+
+Main_Menu_Layout = [
+            [sim.Text('                              Grid Lock         ')],
+            [sim.Text(' \n \n \n \n \n \n \n')],
+           #[sim.Input(key='-IN1-')], #key for mirroring
+            [sim.Text('App/Site Name')],
+            [sim.InputText()],
+            [sim.Text('Username')],
+            [sim.InputText()],      # these are all on different lines to keep them apart
+            [sim.Text('Password')],
+            [sim.InputText()],
+            [sim.Button('Save'), sim.Button('Load')]
+                   ] #end of default window
+# __________________
+
+#----launch window
+window = sim.Window('Grid Lock', Main_Menu_Layout)
+
+# Event Loop to process "events"
+# sim.popup_cancel('popup_cancel')
+while 1 == 1:
+    event, values = window.read()
+    sitename = (event, values[0])
+    username = (event, values[1])
+    password = (event, values[2])
+    usermirror = (str(username)[10:-2])
+    sitemirror = (str(sitename)[10:-2])
+    passmirror = (str(password)[10:-2])
+    if event in (sim.WIN_CLOSED, 'Cancel'): #if closed this happens
+        break
+        # basic debug strings for testing purposes, keep these but comment them out
+    print(event, values)
+    print(str(sitemirror) + ' '  + str(usermirror) + ' ' + str(passmirror))
+    if event == 'Save': #If you press the Save button
+        #window['-IN1-'].update(sitemirror + " \n" + usermirror + " \n" + str(passmirror) + " \n")
+        for i in range(1, 1000):
+            sim.one_line_progress_meter('Saving In Progress', i + 1, 1000, 'Save_Begin', 'Saving your Login Info')
+            # you should put save stuff here.
+    if event == 'Load':
+        PasswordLoad_layout = [
+            [sim.Text('Select Your Login')],
+            [sim.Listbox(values=['Netflix', 'Facebook', 'Youtube', 'Google', 'Youtube'], size=(40, 25))],
+            [sim.Button('Back')]]
+        endflag = False
+        window2 = sim.Window('Grid Lock', PasswordLoad_layout)
+        while endflag == False:
+            event2, values2 = window2.read()
+            if event2 == 'Back':
+                endflag = True
+                window2.close()
+                print("Back")
+
