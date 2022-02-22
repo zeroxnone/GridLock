2abdd92b1512028e1348164bb6e00cee13e44434
J MC <62907451+zeroxnone@users.noreply.github.com>
2022-02-21 22:18:51 -0600

    Update RandNumGen.py

--- RandNumGen.py
+++ RandNumGen.py
@@ -4,5 +4,5 @@ def RandomNumberGenerator_3000to40000():        #defining a function RandomNumbe
 x=random.randrange(3000, 40001)                 #using randrange() to generate in range from 3000 to 40000, default index starting at 0
 return x #returning the value
 
-x=RandomNumberGenerator_3000to40000()                             #c alling the function and saving it in variable "x"
-print("A random number from range 3000 to 40,000 is :", x)        #printing the number stored in "x"
+#x=RandomNumberGenerator_3000to40000()                             #c alling the function and saving it in variable "x"
+#print("A random number from range 3000 to 40,000 is :", x)        #printing the number stored in "x"
