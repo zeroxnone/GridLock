57653fa21480af9eee04f242b1add247830b2357
Hcrooks <94647364+Hcrooks@users.noreply.github.com>
2022-03-30 13:20:16 -0500

    Create Grid_Gen_Key.py

---
+++ Grid_Gen_Key.py
@@ -0,0 +1,11 @@
+import random    #use random prime function need to import random package.
+random_number=[]   #declare  empty list to store generated random numberrs.
+
+def Grid_Gen_Key():
+    for i in range(4): #call prime function random() 4 times 
+           str(random_number.append(random.random()))  #store generated random number as string.
+           
+           
+print("Random number list is : ")
+Grid_Gen_Key()  #call to the function
+print(random_number)  #display list of random number.
